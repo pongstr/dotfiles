4d0d289d4814156519f66637b0f42e257687f90c
Pongstr <twiz.tickler@gmail.com>
2014-02-06 08:13:14 +0800

    updated bootstrap.sh, added .hushlogin file and moved command from .terminal to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,42 +4,86 @@
 # https://developer.apple.com/xcode/
 # https://developer.apple.com/downloads/
 
+# Function to check if a package exists
+check () { type -t "${@}" > /dev/null 2>&1; }
+
+# Function to install Homebrew Formulas:
+install_formula () {
+  echo "Installing Homebrew Packages:"
+  echo "  ➜ libyaml"
+  brew install libyaml
+
+  echo "  ➜ openssl"
+  brew install openssl
+
+  echo "  ➜ git"
+  brew install git
+
+  echo "  ➜ node"
+  brew install node
+
+  echo "  ➜ zsh"
+  brew install zsh
+  echo "  ➜ vim (overriding system vim)"
+  brew install vim --override-system-vi
+
+  # Cleanup
+  echo "Cleaning up Homebrew intallation..."
+  brew cleanup  
+}
+
+
+# Install Hushlogin
+echo "Install hushlogin"
+echo "  - Disable the system copyright notice, the date and time of the last login."
+echo "    more info at @mathiasbynens/dotfiles http://goo.gl/wZBM80"
+echo ""
+cp -r ".hushlogin" $HOME/.hushlogin
+
+
 # Install Homebrew
 # ---------------------------------------------------------------------------
-ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
+echo "Checking if Homebrew is installed..."
 
-# Run Brew doctor before anything else
-brew doctor
+if check brew; then
+  echo "Awesome! Homebrew is installed! Now updating..."
+  brew upgrade
+  brew update
+fi
 
-# Install Homebrew Formulas:
-echo "Installing Homebrew Packages:"
-echo "  ➜ libyaml"
-brew install libyaml
+if ! check brew; then
+  # Download and install homebrew
+  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
 
-echo "  ➜ openssl"
-brew install openssl
+  # Run Brew doctor before anything else
+  brew doctor
+fi
 
-echo "  ➜ git"
-brew install git
+# Install Homebrew Formulas
+while true; do
+  read -p "Would you like to install Homebrew formulas? [y/n] " answer
+  case $answer in
+    [y]* ) install_formula; break;;
+    [n]* ) break;;
+    * ) echo "Please answer Y or N.";;
+  esac
+done
 
-echo "  ➜ node"
-brew install node
-
-echo "  ➜ zsh"
-brew install zsh
-
-echo "  ➜ vim (overriding system vim)"
-brew install vim --override-system-vi
-
-# Cleanup
-echo "Cleaning up Homebrew intallation..."
-brew cleanup
 
 # Install RVM
 # ---------------------------------------------------------------------------
-echo "Installing RVM..."
-\curl -L https://get.rvm.io | bash
+if check rvm; then
+  echo "Awesome! RVM is installed! Now updating..."
+  rvm get head
+  rvm reload
+  rvm get stable
+fi
 
-# Restart Terminal for RVM to take effect
-echo "bootstrapping complete! quitting terminal..."
-killall Terminal
+if ! check rvm; then
+  echo "Installing RVM..."
+  \curl -L https://get.rvm.io | bash
+
+  # Restart Terminal for RVM to take effect
+  echo "bootstrapping complete! quitting terminal..."
+  killall Terminal
+fi
