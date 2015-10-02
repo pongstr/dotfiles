7d647b4647d10651ef3eac818855759db84a9268
Pongstr <twiz.tickler@gmail.com>
2015-10-02 22:30:45 +0800

    init

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,173 +1,53 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-echo ""
-echo "      ___       ___          ___          ___                   ___      "
-echo "     /  /\     /__/\        /  /\        /  /\         ___     /  /\     "
-echo "    /  /::\    \  \:\      /  /:/_      /  /:/_       /  /\   /  /::\    "
-echo "   /  /:/\:\    \  \:\    /  /:/ /\    /  /:/ /\     /  /:/  /  /:/\:\   "
-echo "  /  /:/~/:/_____\__\:\  /  /:/_/::\  /  /:/ /::\   /  /:/  /  /:/~/:/   "
-echo " /__/:/ /://__/::::::::\/__/:/__\/\:\/__/:/ /:/\:\ /  /::\ /__/:/ /:/___ "
-echo " \  \:\/:/ \  \:\~~\~~\/\  \:\ /~~/:/\  \:\/:/~/://__/:/\:\\  \:\/:::::/ "
-echo "  \  \::/   \  \:\  ~~~  \  \:\  /:/  \  \::/ /:/ \__\/  \:\\  \::/~~~~  "
-echo "   \  \:\    \  \:\       \  \:\/:/    \__\/ /:/       \  \:\\  \:\      "
-echo "    \  \:\    \  \:\       \  \::/       /__/:/         \__\/ \  \:\     "
-echo "     \__\/     \__\/        \__\/        \__\/                 \__\/     "
-echo ""
-echo "        ..........................................................       "
-echo "        . Dotfiles 0.1.15 (Pongstr) for setting up OSX Workspace .       "
-echo "        .      https://github.com/pongstr/dotfiles.git           .       "
-echo "        ..........................................................       "
-echo ""
+printf "%s" $'\e[1;32m
+  ██████╗  ██████╗ ███╗   ██╗ ██████╗ ███████╗████████╗██████╗
+  ██╔══██╗██╔═══██╗████╗  ██║██╔════╝ ██╔════╝╚══██╔══╝██╔══██╗
+  ██████╔╝██║   ██║██╔██╗ ██║██║  ███╗███████╗   ██║   ██████╔╝
+  ██╔═══╝ ██║   ██║██║╚██╗██║██║   ██║╚════██║   ██║   ██╔══██╗
+  ██║     ╚██████╔╝██║ ╚████║╚██████╔╝███████║   ██║   ██║  ██║
+  ╚═╝      ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝ ╚══════╝   ╚═╝   ╚═╝  ╚═╝\e[1;31m
+      Dotfiles v0.1.16 https://github.com/pongstr/dotfiles
 
-# To run this, you must download & install the latest Xcode and Commandline Tools
-# https://developer.apple.com/xcode/
-# https://developer.apple.com/downloads/
+\e[0;1m'
 
-echo ""
-echo "  To run this, you must download & install the latest Xcode and Commandline Tools"
-echo "    > https://developer.apple.com/xcode/"
-echo "    > https://developer.apple.com/downloads/"
-xcode-select --install
-
-# Function to check if a package exists
-check () { type -t "${@}" > /dev/null 2>&1; }
-
-# Function to install Homebrew Formulas:
-install_formula () {
-  DOTFILE=$(pwd)
-
-  echo ""
-  echo "Installing Homebrew Packages:"
-
-  echo ""
-  echo "  ➜ dnsmasq"
-  brew install dnsmasq
-
-  echo ""
-  echo "  ➜ git"
-  brew install git
-
-  echo ""
-  echo "  ➜ libyaml"
-  brew install libyaml
-
-  echo ""
-  echo "  ➜ mongodb"
-  brew install mongo
-  mkdir $HOME/.mongodb-data
-
-  echo ""
-  echo "  ➜ nginx"
-  brew install nginx
-
-  echo ""
-  echo "  ➜ node"
-  brew install node
-
-  echo ""
-  echo "  ➜ openssl"
-  brew install openssl
-
-  echo ""
-  echo "  ➜ python"
-  brew install python
-
-  echo ""
-  echo "  ➜ vim (overriding system vim)"
-  brew install vim --override-system-vi
-
-  echo ""
-  echo "  ➜ zsh"
-  brew install zsh
-
-  # Cleanup
-  echo ""
-  echo "Cleaning up Homebrew installation..."
-  brew cleanup
-
-  yes | cp -rf $DOTFILE/bin/shell/.bashrc $HOME/.bashrc
-  yes | cp -rf $DOTFILE/bin/shell/.bash_alias $HOME/.bash_alias
-  yes | cp -rf $DOTFILE/bin/shell/.bash_profile $HOME/.bash_profile
-
-  echo "Installing Caskroom, Caskroom versions, Caskroom Fonts and Brew Services"
-  brew install caskroom/cask/brew-cask
-  brew tap homebrew/services
-  brew tap caskroom/versions
-  brew tap caskroom/fonts
-
-  # Make /Applications the default location of apps
-  export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-
-  echo ""
-  echo "Installing monospace fonts... "
-  brew cask install font-droid-sans-mono
-  brew cask install font-ubuntu
-  brew cleanup
-
-
+brew_formula () {
+  if brew info "${@}" | grep "Not installed" > /dev/null; then
+    echo "  --> Installing ${@}..."
+    brew install "${@}"
+  else
+    echo "  --> ${@} is already installed."
+  fi
 }
 
+install () {
+  packages=(
+    dnsmasq
+    git
+    libyaml
+    mongo
+    nginx
+    nodenv
+    openssl
+    python
+    rbenv
+    "vim --override-system-vi"
+    wget
+    zsh
+  )
 
-# Install Hushlogin
-echo ""
-echo "Install hushlogin"
-echo "  - Disable the system copyright notice, the date and time of the last login."
-echo "    more info at @mathiasbynens/dotfiles http://goo.gl/wZBM80"
-echo ""
-yes | cp -rf "$DOTFILE/.hushlogin" $HOME/.hushlogin
+  for pkg in "${packages[@]}"
+  do
+    if [ $# -eq 0 ]; then
+      echo $1
+      echo $pkg
+    fi
+    # if brew info "${pkg[@]}" | grep "Not installed" > /dev/null; then
+    #   brew install "${pkg[@]}"
+    # else
+    #   echo "  --> ${@} is already installed."
+    # fi
+  done
+}
 
-
-# Install Homebrew
-# ---------------------------------------------------------------------------
-echo ""
-echo "Checking if Homebrew is installed..."
-
-if check brew; then
-  echo "Awesome! Homebrew is installed! Now updating..."
-  echo ""
-  brew upgrade
-  brew update --all
-fi
-
-if ! check brew; then
-  echo "Download and install homebrew"
-  echo ""
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-  # Run Brew doctor before anything else
-  brew doctor
-fi
-
-# Install Homebrew Formulas
-while true; do
-  read -p "Would you like to install Homebrew formulas? [y/n] " answer
-  echo ""
-  case $answer in
-    [y/Y]* ) install_formula; break;;
-    [n/N]* ) break;;
-    * ) echo "Please answer Y or N.";;
-  esac
-done
-
-
-# Install RVM
-# ---------------------------------------------------------------------------
-
-echo ""
-echo "Installing RVM and latest version of Ruby"
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
-
-echo ""
-echo "  >   Update OSX SSL Certificates"
-rvm osx-ssl-certs update all
-
-echo "  >   Auto update SSL Certificates"
-rvm osx-ssl-certs cron install
-
-sleep 1
-rvm reload
-
-# Restart Terminal for RVM to take effect
-echo ""
-echo "bootstrapping complete! quitting terminal..."
-killall Terminal
+install
